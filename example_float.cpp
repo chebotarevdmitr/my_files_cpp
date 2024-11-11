631ee921dff9df113b86cd9a948edaac7d9740a3
Dmitriy Chebotarev <4ebotarov_dim@inbox.ru>
2024-11-11 14:30:28 -0500

    edded tutorial_geme

--- example_float.cpp
+++ example_float.cpp
@@ -1,7 +1,10 @@
 #include <stdio.h>
 
   int main(){
+    float Y ;
     printf("hello Vasya \n");
-    printf("Ivam salyam\n");
+    printf("\t Input Y : ") ; //vivodim soobhenie
+    scanf("%f",&Y) ; //vvodim znachenie peremennoy Y
+    printf("Znachenie peremennoy Y = %f",Y); 
     return 0;
 }
